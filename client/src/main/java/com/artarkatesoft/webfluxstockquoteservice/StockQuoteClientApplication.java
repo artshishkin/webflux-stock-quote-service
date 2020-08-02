4fd3e476746a5552655bdcee7a0cce585eb91606
admin <d.art.shishkin@gmail.com>
2020-08-02 16:05:25 +0300

    added client module to multi-module project. closes #11

---
+++ client/src/main/java/com/artarkatesoft/webfluxstockquoteservice/StockQuoteClientApplication.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.webfluxstockquoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockQuoteClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockQuoteClientApplication.class, args);
+    }
+
+}
---
+++ client/src/test/java/com/artarkatesoft/webfluxstockquoteservice/StockQuoteClientApplicationTest.java
@@ -0,0 +1,10 @@
+package com.artarkatesoft.webfluxstockquoteservice;
+
+import org.junit.jupiter.api.Test;
+
+class StockQuoteClientApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
