2aede0ea154811332c2983588e3956c63c8a84dc
Art <d.art.shishkin@gmail.com>
2020-08-02 22:49:16 +0300

    Tailable Cursors with MongoDB (51). closes #16

--- client/src/main/java/com/artarkatesoft/webfluxstockquoteservice/client/QuoteRunner.java
+++ client/src/main/java/com/artarkatesoft/webfluxstockquoteservice/client/QuoteRunner.java
@@ -1,19 +1,23 @@
 package com.artarkatesoft.webfluxstockquoteservice.client;
 
+import com.artarkatesoft.webfluxstockquoteservice.client.model.Quote;
+import com.artarkatesoft.webfluxstockquoteservice.client.repository.QuoteRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
-//@Component
+@Component
 @Slf4j
 @RequiredArgsConstructor
 public class QuoteRunner implements CommandLineRunner {
 
-    private final StockQuoteClient stockQuoteClient;
+    private final QuoteRepository quoteRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        stockQuoteClient.getQuoteStream()
-                .subscribe(quote -> log.info("Received quote is {}", quote));
+        Flux<Quote> tailableCursorQuoteFlux = quoteRepository.findWithTailableCursorBy();
+        tailableCursorQuoteFlux.subscribe(quote -> log.debug("Quote from DB: {}", quote));
     }
 }
--- client/src/main/java/com/artarkatesoft/webfluxstockquoteservice/client/repository/QuoteRepository.java
+++ client/src/main/java/com/artarkatesoft/webfluxstockquoteservice/client/repository/QuoteRepository.java
@@ -2,6 +2,10 @@ package com.artarkatesoft.webfluxstockquoteservice.client.repository;
 
 import com.artarkatesoft.webfluxstockquoteservice.client.model.Quote;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
 
 public interface QuoteRepository extends ReactiveMongoRepository<Quote, String> {
+    @Tailable
+    Flux<Quote> findWithTailableCursorBy();
 }
